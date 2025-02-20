3f1173a3b8285f00a6e5a3f09806ef359b074ba9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-19 22:41:27 -0300

    aula 1 ex 1 e 2

---
+++ Aula 1 - 19-02/ex 1.py
@@ -0,0 +1,25 @@
+potencia = float(input("qual a potencia?"))
+voltagem = float(input("qual a voltagem"))
+corrente = potencia/voltagem
+print("o valor da corrente é " , corrente)
+
+
+
+
+resis = float(input("qual a resistencia"))
+corrente = float(input("qual a corrente"))
+voltagem = corrente *resis
+print("o valor da voltagem é " , voltagem)
+
+
+
+
+resistencia = float(input("qual a resistencia"))
+correnteEletrica = float(input("qual a corrente"))
+
+pot = resis * (correnteEletrica ** 2)
+print("o valor da potencia é " , pot)
+
+
+
+
---
+++ Aula 1 - 19-02/ex 2.py
@@ -0,0 +1,10 @@
+idade = 16
+
+if idade < 12:
+    print("Criança")
+
+elif 12 <= idade > 18:
+    print("adolescente")
+
+else:
+    print("adulto")
